18d9a3a60966cfa63105fb8ffc337fb0a193bc2e
rkobus <rkob7319@gmail.com>
2018-12-09 14:51:42 +0100

    Aplikacja

---
+++ PocztaOnline.Aplikacja/Pages/Elementy/Nawigacja.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PocztaOnline.Aplikacja.Pages.Elementy
+{
+    public class NawigacjaModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
